791abfa0e91fa8728767d331897656a73849477e
Jarod Kober <jarod.development@gmail.com>
2022-03-15 16:11:04 -0700

    Defined Webpack Configuration

---
+++ webpack.config.dev.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+	devServer: {
+		historyApiFallback: true,
+		overlay: true,
+		stats: 'minimal'
+	},
+	devTool: 'cheap-module-source-map',
+	entry: './src/index',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				exclude: /node_modules/,
+				test: /\.(js|jsx)$/,
+				use: ['babel-loader']
+			},
+			{
+				test: /(\.css)$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			favicon: 'src/favicon.ico',
+			template: 'src/index.html'
+		})
+	],
+	target: 'web'
+};
