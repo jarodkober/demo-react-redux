f4c0b8cd4eba41747612af6827b9242ce36427b5
Jarod Kober <jarod.development@gmail.com>
2022-03-15 16:36:38 -0700

    Configured ESLint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,7 +18,7 @@ module.exports = {
 			{
 				exclude: /node_modules/,
 				test: /\.(js|jsx)$/,
-				use: ['babel-loader']
+				use: ['babel-loader', 'eslint-loader']
 			},
 			{
 				test: /(\.css)$/,
