68afa6f30d7c5a3a95934cb59371ab4eb9913c9d
Jarod Kober <jarod.development@gmail.com>
2022-03-22 11:50:38 -0700

    Instantiate Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import App from './components/App.js';
+import configureStore from './redux/configureStore';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+const store = configureStore();
+
 render(
-	<Router>
-		<App />
-	</Router>,
+	<ReduxProvider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</ReduxProvider>,
 	document.getElementById('app')
 );
