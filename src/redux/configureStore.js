f11450343a89fd2410eb56663f21308fa454e367
Jarod Kober <jarod.development@gmail.com>
2022-03-22 11:44:42 -0700

    Created Store
    * Added redux-immutable-state-invariant to ensure we don't update state
    * Added support for Redux dev tools

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from './reducers';
+
+export default function configureStore(initialState) {
+	// Add support for Redux dev tools
+	const composeEnhancers =
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+	return createStore(
+		rootReducer,
+		initialState,
+		composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+	);
+}
